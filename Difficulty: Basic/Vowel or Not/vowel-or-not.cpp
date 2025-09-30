70ab105578b06473a148f65eeaf27363b569f225
rishabhjha05 <rishabhjha012043@gmail.com>
2025-09-30 21:40:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Vowel or Not/vowel-or-not.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    string isVowel(char c) {
+        // code here
+        switch(c){
+            case 'a':
+                return "YES";
+            case 'e':
+                return "YES";
+            case 'i':
+                return "YES";
+            case 'o':
+                return "YES";
+            case 'u':
+                return "YES";
+            case 'A':
+                return "YES";
+            case 'E':
+                return "YES";
+            case 'I':
+                return "YES";
+            case 'O':
+                return "YES";
+            case 'U':
+                return "YES";
+            default:
+            return "NO";
+        }
+    }
+};
